de4acf3c8a44d230ba3e151416156cf86063cb94
luzumsuzadam <cengizulutas123123@gmail.com>
2025-07-11 16:22:57 +0300

    newsletter

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -3,14 +3,19 @@ import { ReactNode } from "react";
 // Components
 import StoreHeader from "@/components/store/layout/header/header";
 import CategoriesHeader from "@/components/store/layout/categories-header/categories-header";
+import Footer from "@/components/store/layout/footer/footer";
 
 export default function StoreLayout({children}: {children: ReactNode}) {
   return (
-    <div>
-      <StoreHeader />
-      <CategoriesHeader />
-      <main>{children}</main>
-      <div>Footer</div>
+    <div className="min-h-screen flex flex-col">
+      <header>
+        <StoreHeader />
+        <CategoriesHeader />
+      </header>
+      <main className="flex-1">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
---
+++ components/store/layout/footer/footer.tsx
@@ -0,0 +1,12 @@
+import Newsletter from "./newsletter";
+
+export default function Footer() {
+  return (
+    <div className="w-full bg-white">
+      <Newsletter />
+      {/* Footer Links */}
+      {/* Rights */}
+      Footer
+    </div>
+  )
+}
