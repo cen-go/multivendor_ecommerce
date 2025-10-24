e518789842337ed32477cc678b5af93a76c2c646
luzumsuzadam <cengizulutas123123@gmail.com>
2025-10-24 16:08:16 +0300

    progress bar

--- components/store/forms/apply-seller/apply-seller-form.tsx
+++ components/store/forms/apply-seller/apply-seller-form.tsx
@@ -1,11 +1,39 @@
 "use client"
 
+import { StoreType } from "@/lib/types";
 import { useState } from "react";
+import Instructions from "./instructions";
+import ProgressBar from "./progress-bar";
 
 export default function ApplySellerMultiForm() {
   const [step, setStep] = useState<number>(1);
-  
+  const [formData, setFormData] = useState<StoreType>({
+    name: "",
+    description: "",
+    email: "",
+    phone: "",
+    url: "",
+    logo: "",
+    cover: "",
+    defaultShippingService: "",
+    defaultShippingFeePerItem: undefined,
+    defaultShippingFeeForAdditionalItem: undefined,
+    defaultShippingFeePerKg: undefined,
+    defaultShippingFeeFixed: undefined,
+    defaultDeliveryTimeMin: undefined,
+    defaultDeliveryTimeMax: undefined,
+    returnPolicy: "",
+  });
+
   return (
-    <div></div>
+    <div className="grid lg:grid-cols-[400px_1fr]">
+      {/* instructions */}
+      <Instructions />
+      <div className="relative p-5 w-full">
+        {/* Progress bar */}
+        <ProgressBar step={step} />
+        {/* Steps */}
+      </div>
+    </div>
   )
 }
